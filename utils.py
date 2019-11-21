eb2c0c41fd03c074f09450d4e357b20b5c46c09b
Sri Sai Chandu chandu kamanuru <kamanurusrisaichandu@gmail.com>
2019-11-21 00:16:23 -0700

    Finished Non-Max Suppression .Picks left most bbox.

--- utils.py
+++ utils.py
@@ -29,20 +29,44 @@ def non_max_suppression(bboxes, min_IOU=.75):
     # bboxes are arrays with [x1,y1,x2,y2] co-ordinates for each bounding box that is predicted 
     # x1,y1 and x2,y2 are co-ordinates of top left and bottom right points of the bounding box
     
-    if len(bboxes) >= 0:
-        x1 = bboxes[:,0]
-        y1 = bboxes[;,1]
-        x2 = bboxes[:,2]
-        y2 = bboxes[;,3]
+    # if bboxes is empty, return an empty list
+    if len(bboxes) == 0: return []
         
-        chosen_bbox = []
-       
-        area = (x2 - x1) * (y2 - y1)
-    else:
-        return []
-    
-    
-    return
+    if bboxes.dtype.kind == "i": bboxes = bboxes.astype("float")
+
+    # initialize the result list of selected bboxes
+    result = []
+    # divide coordinates of the bounding boxes
+    x1,y1,x2,y2 = bboxes[:, 0],bboxes[:, 1],bboxes[:, 2],bboxes[:, 3]
+
+    # compute each bbox area 
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    # sort indices of x1 to compare with others.
+    left_most_arg = np.argsort(x1)[::-1]
+
+    # looping around x1 indices
+    while len(left_most_arg) > 0:
+        
+        last = left_most_arg.shape[0] - 1
+        k = left_most_arg[last]
+        result.append(k)
+
+        i_x1 = np.maximum(x1[k], x1[left_most_arg[:last]])
+        i_y1 = np.maximum(y1[k], y1[left_most_arg[:last]])
+        i_x2 = np.minimum(x2[k], x2[left_most_arg[:last]])
+        i_y2 = np.minimum(y2[k], y2[left_most_arg[:last]])
+
+        # Find intersection width and height
+        width = np.maximum(0, i_x2 - i_x1 + 1)
+        height = np.maximum(0, i_y2 - i_y1 + 1)
+
+        #ratio of overlap
+        IOU = (width * height) / area[left_most_arg[:last]]
+
+        # remove overlaps
+        left_most_arg = np.delete(left_most_arg, np.concatenate(([last],np.where(IOU > min_IOU)[0])))
+
+    return bboxes[result].astype("int")
 
 
 def non_max_suppression_numpy(bboxes):
