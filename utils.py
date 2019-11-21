0b3a0b458d1ec480ecdb2722fcd1242c886d13d7
jmad-1995 <46641373+jmad-1995@users.noreply.github.com>
2019-11-20 18:50:16 -0700

    v1 jmad

--- utils.py
+++ utils.py
@@ -24,8 +24,24 @@ def objects_metrics(classes_prediction, bboxes_prediction, classes_targets, bbox
     """
     return
 
-
+ 
 def non_max_suppression(bboxes, min_IOU=.75):
+    # bboxes are arrays with [x1,y1,x2,y2] co-ordinates for each bounding box that is predicted 
+    # x1,y1 and x2,y2 are co-ordinates of top left and bottom right points of the bounding box
+    
+    if len(bboxes) >= 0:
+        x1 = bboxes[:,0]
+        y1 = bboxes[;,1]
+        x2 = bboxes[:,2]
+        y2 = bboxes[;,3]
+        
+        chosen_bbox = []
+       
+        area = (x2 - x1) * (y2 - y1)
+    else:
+        return []
+    
+    
     return
 
 
