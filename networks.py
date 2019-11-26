7bd5523aa0b559f3a744c1171c83b9f86b39a380
alexmagsam <alexmagsam@outlook.com>
2019-11-26 16:51:50 -0700

    predict method and predict video modifications

--- networks.py
+++ networks.py
@@ -36,7 +36,7 @@ class MultiPurposeCNN(nn.Module):
 
         return {'depths': depth, 'objects': detections}
 
-    def predict(self, image):
+    def predict(self, image, device='cpu'):
         self.eval()
 
         # Convert to float
@@ -47,31 +47,43 @@ class MultiPurposeCNN(nn.Module):
         aspect_ratio = 1280 / 384
         if image.shape[1] / image.shape[0] > aspect_ratio:
             pad_x = 0
-            pad_y = image.shape[1] / aspect_ratio - image.shape[0]
+            pad_y = int(image.shape[1] / aspect_ratio - image.shape[0])
         else:
-            pad_x = image.shape[0] * aspect_ratio - image.shape[1]
+            pad_x = int(image.shape[0] * aspect_ratio - image.shape[1])
             pad_y = 0
         padded = np.pad(image, ((0, pad_y), (0, pad_x), (0, 0)), constant_values=1e-3,
                         mode='constant')
 
         # Resize
-        resized = resize(padded, (1280, 384), mode='constant', cval=1e-3, anti_aliasing=False)
+        resized = resize(padded, (384, 1280), mode='constant', cval=1e-3, anti_aliasing=False).astype(np.float32)
 
         # Predict on image plus mirror image
         image_flipped = np.flip(resized, axis=1)
         image_tensor = torch.from_numpy(np.stack([resized, image_flipped], axis=0).copy()).permute(0, 3, 1, 2)
 
         # Forward pass + average of two predictions
-        predictions = self.forward(image_tensor)
+        predictions = self.forward(image_tensor.to(device))
         depth = predictions['depths'].squeeze()
         depth = torch.mean(torch.stack([depth[0], torch.flip(depth[1], dims=[1])]), dim=0)
 
         # Post-process
         depth = depth.detach().cpu().numpy()
         depth = resize(depth, resized.shape[:2], mode='constant', cval=1e-3, anti_aliasing=False)
-        depth = depth[:image.shape[0], :image.shape[1]]
 
-        return depth
+        # Crop
+        x, y = (int(pad_x * resized.shape[1] / padded.shape[1]),
+                int(pad_y * resized.shape[0] / padded.shape[0]))
+
+        # print("image shape: ", image.shape)
+        # print('resized shape: ', resized.shape)
+        # print("padded shape: ", padded.shape)
+        # print("pad x", pad_x)
+        # print("pad y", pad_y)
+
+        depth = depth[:384-y, :1280-x]
+        resized = resized[:384-y, :1280-x, :]
+
+        return resized, depth
 
 
 class MobileNetV2(nn.Module):
--- predict_video.py
+++ predict_video.py
@@ -1,7 +1,11 @@
 import argparse
 import torch
 import networks
+import skvideo
+skvideo.setFFmpegPath(r'C:\Program Files\FFmpeg\bin')
 import skvideo.io as io
+import numpy as np
+import matplotlib.cm as cm
 
 # Argument parser
 parser = argparse.ArgumentParser()
@@ -9,19 +13,34 @@ parser.add_argument('-video_in', required=True, help='Path to the input video fi
 parser.add_argument('-video_out', required=True, help='Path to the output video file')
 parser.add_argument('-saved_model', required=True, help='Path to the trained model')
 parser.add_argument('-device', default=0, type=int, help='CUDA device')
-parser.add_argument('-axis', default=0, type=int, help='Axis to stack RGB image and depth prediction')
+parser.add_argument('-axis', default=1, type=int, help='Axis to stack RGB image and depth prediction')
 args = parser.parse_args()
+
+# CUDA device
+device = "cuda:{}".format(args.device) if torch.cuda.is_available() else 'cpu'
 
 # Create the reader and writer
 reader = io.FFmpegReader(filename=args.video_in)
 writer = io.FFmpegWriter(filename=args.video_out)
 
-#
-model = networks.MultiPurposeCNN()
+# Trained model
+model = networks.MultiPurposeCNN().to(device)
 model.load_state_dict(torch.load(args.saved_model))
 model.eval()
 
-for idx in reader.getShape()[0]:
+cmapper = cm.get_cmap('plasma')
+idx = 0
+for frame_in in reader.nextFrame():
+    idx += 1
+    print("Predicting frame {:05d} of {}".format(idx, reader.getShape()[0]))
 
-    frame_in = reader.nextFrame()
+    rgb, depth = model.predict(frame_in, device)
+
+    depth_color = cmapper(np.clip(depth / 11., 0., 1.))[..., :3]
+
+    frame_out = np.concatenate([rgb, depth_color], axis=args.axis)
+
+    writer.writeFrame(np.uint8(frame_out * 255.))
+
+reader.close()
 
